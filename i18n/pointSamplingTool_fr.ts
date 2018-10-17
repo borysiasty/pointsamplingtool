f51223718ca158450285bd80edf0e485be522466
SPeillet <peillet.seb@gmail.com>
2018-10-17 17:46:13 +0200

    French translation

---
+++ i18n/pointSamplingTool_fr.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="17"/>
+        <source>Point Sampling Tool</source>
+        <translation>Point Sampling Tool</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="34"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="48"/>
+        <source>Layer containing sampling points:</source>
+        <translation>Couche vecteur contenant les points :</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="68"/>
+        <source>Layers with fields/bands to get values from:</source>
+        <translation>Cocuhes avec les champs/bandes d'où extraire les valeurs :</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="99"/>
+        <source>Output point vector layer:</source>
+        <translation>Couche de points en sortie</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="110"/>
+        <source>Browse</source>
+        <translation>Naviguer</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="119"/>
+        <source>Add created layer to the map</source>
+        <translation>Ajouter la couche créée à la carte</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="130"/>
+        <source>Fields</source>
+        <translation>Champs</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="167"/>
+        <source>source</source>
+        <translation>source</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="172"/>
+        <source>name</source>
+        <translation>nom</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="181"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="190"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;The Point Sampling Tool Plugin collects polygon attributes and raster values from multiple layers at specified sampling points. You need a point layer with locations of sampling points and at least one polygon or raster layer to probe values from. The plugin creates a new point layer with locations given by the sampling points and attributes taken from all the underlying polygons or/and raster cells.&lt;/p&gt;&lt;p&gt;Please use Control and Shift keys in order to select multiple columns and bands.&lt;/p&gt;&lt;p&gt;NOTE: This tool is not compatible with mulitipoint sources, unless each multipoint contains exactly one point. Using multipoint samples that contain more points in multipoints may produce unreliable results.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Point Sampling Tool Plugin récupère les valeurs de polygones ou de raster values situé sous un échantillon de points. Une couche de point est nécessaire avec la localisation des points ainsi d'une couche de polygones ou un raster pour récupérer la valeur. Le plugin crée une nouvelle couche de point avec la localisation de la couche de points en entrée et les attributs récupérés de toutes les couches sélectionnés (polygones ou raster).&lt;/p&gt;&lt;p&gt;Veuillez utiliser les touches Ctrl et Shift pour sélectionner plusieurs champs ou bandes.&lt;/p&gt;&lt;p&gt;NOTE: Cet outils n'est pas compatible avec les couches multipoints, à moins que chacun n'en contiennent qu'un. Utiliser une couche multipoints avec plusieurs points par entité ne renvoie pas de résultats appropriés.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="233"/>
+        <source>Status:</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../pointSamplingToolUi.ui" line="260"/>
+        <source>Complete the input fields and press OK...</source>
+        <translation>Compléter les champs en entrée et appuyer sur OK</translation>
+    </message>
+</context>
+</TS>
