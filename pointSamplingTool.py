fba9be3fc17c931386847e3f1eec3cf51642795d
SPeillet <peillet.seb@gmail.com>
2018-10-18 13:10:49 +0200

    add translator

--- pointSamplingTool.py
+++ pointSamplingTool.py
@@ -19,6 +19,7 @@
 # ***************************************************************************
 
 from builtins import object
+from PyQt5.QtCore import QSettings, QTranslator, QCoreApplication
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction
 from qgis.core import *
@@ -29,26 +30,38 @@ from . import doPointSamplingTool
 class pointSamplingTool(object):
 
  def __init__(self, iface):
-  self.iface = iface
+	self.iface = iface
+	locale = QSettings().value('locale/userLocale')[0:2]
+	locale_path = os.path.join(
+		self.plugin_dir,
+		'i18n',
+		'AutomaTracks_{}.qm'.format(locale))
+
+	if os.path.exists(locale_path):
+		self.translator = QTranslator()
+		self.translator.load(locale_path)
+
+	if qVersion() > '4.3.3':
+		QCoreApplication.installTranslator(self.translator)
 
 
  def initGui(self):
-  # create action
-  self.action = QAction(QIcon(":/plugins/pointSamplingTool/pointSamplingToolIcon.png"), "Point sampling tool", self.iface.mainWindow())
-  self.action.setWhatsThis("Collects polygon attributes and raster values from multiple layers at specified sampling points")
-  self.action.triggered.connect(self.run)
-  # add toolbar button and menu item
-  self.iface.addToolBarIcon(self.action)
-  self.iface.addPluginToMenu("&Analyses", self.action)
+	# create action
+	self.action = QAction(QIcon(":/plugins/pointSamplingTool/pointSamplingToolIcon.png"), "Point sampling tool", self.iface.mainWindow())
+	self.action.setWhatsThis("Collects polygon attributes and raster values from multiple layers at specified sampling points")
+	self.action.triggered.connect(self.run)
+	# add toolbar button and menu item
+	self.iface.addToolBarIcon(self.action)
+	self.iface.addPluginToMenu("&Analyses", self.action)
 
 
  def unload(self):
-  # remove the plugin menu item and icon
-  self.iface.removePluginMenu("&Analyses",self.action)
-  self.iface.removeToolBarIcon(self.action)
+	# remove the plugin menu item and icon
+	self.iface.removePluginMenu("&Analyses",self.action)
+	self.iface.removeToolBarIcon(self.action)
 
 
  def run(self):
-  # create and show a configuration dialog or something similar
-  dialoga = doPointSamplingTool.Dialog(self.iface)
-  dialoga.exec_()
+	# create and show a configuration dialog or something similar
+	dialoga = doPointSamplingTool.Dialog(self.iface)
+	dialoga.exec_()
